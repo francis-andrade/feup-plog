58c5567f1a7cdcc1a30b162c79329c451cda98f5
danrpinho <up201505302@fe.up.pt>
2017-10-13 19:17:05 +0100

    oh hey it works

--- display.pl
+++ display.pl
@@ -1,22 +1,22 @@
-Display_matrix(8, []).
-Display_matrix(N, [L|T]) :- 
-    N1 is N+1, N <= 7, 
-    Display_line(L, N), 
-    Display_matrix(N1, T).
+display_matrix(8, []).
+display_matrix(N, [L|T]) :- 
+    N1 is N+1, N =< 7, 
+    display_line(L, N), 
+    display_matrix(N1, T).
 
 %imprime a linha
-Display_line(L, N):-
-    put_char(N), put_char(' '), 
-    put_char('|'), Display_line(L).
+display_line(L, N):-
+    write(N), put_char(' '), 
+    put_char('|'), display_line(L).
 
 %ultimo elemento da linha
-Display_line([X]) :- put_char(X), put_char('|'), put_code(10). 
-Display_line([X|R]) :- put_char(X), put_char(' '), Display_line(R).
+display_line([X]) :- write(X), write('|'), put_code(10). 
+display_line([X|R]) :- write(X), write(' '), display_line(R).
 
-Display_top() :- write ('   1 2 3 4 5 6 7'), put_code(10).
-Display_edge() :- write('  +-------------+'), put_code(10).
+display_top :- write('   1 2 3 4 5 6 7'), put_code(10).
+display_edge :- write('  +-------------+'), put_code(10).
 
-Display_board(X):- Display_top(), Display_edge(), Display_matrix(1, X), Display_edge().
+display_board(X):- display_top, display_edge, display_matrix(1, X), display_edge.
 
 /*
    1 2 3 4 5 6 7
