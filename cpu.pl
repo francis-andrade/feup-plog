1c4047f47f798d767d73ca2ba41d0701d5126802
francis-andrade <up201503481@fe.up.pt>
2017-11-09 19:27:00 +0000

    bugs corrected and tests added

--- cpu.pl
+++ cpu.pl
@@ -37,31 +37,47 @@ valid_move(Board, Player,  Move, NewBoard) :-
 	valid_move(Trans_Board, Player, ['right',N], Trans_NewBoard),
 	transpose(Trans_NewBoard,NewBoard),!.
 	
-valid_move(Board, Player, Move, 0);
+valid_move(Board, Player, Move, 0).
 
-valid_moves_make_list_aux(Board, Player,Move, N, List):-
-	valid_move(Board, Player, Move, NewBoard),
-	Move=[Edge, X],	
-	nth1(N, List, NewBoard),
-	X1 is X + 1, N1 is N + 1,
-	(X = 7; valid_moves_make_list_aux(Board, Player, [Edge, X + 1], N + 1, List)).
 
-valid_moves_make_list(Board, Player, List):-
-	valid_moves_make_list_aux(Board, Player,['left',1], 1, List),
-	valid_moves_make_list_aux(Board, Player,['right',1], 8, List),
-	valid_moves_make_list_aux(Board, Player,['up',1], 15, List),
-	valid_moves_make_list_aux(Board, Player,['down',1], 22, List).
+
+valid_moves_make_list_aux(Board, Player, Move, ListOfMoves_0):-
+	Move=['left', 8],!,
+	valid_moves_make_list_aux(Board, Player, ['right',1], ListOfMoves_0).
+
+valid_moves_make_list_aux(Board, Player, Move, ListOfMoves_0):-
+        Move=['right', 8],!,
+        valid_moves_make_list_aux(Board, Player, ['up',1], ListOfMoves_0).
+
+valid_moves_make_list_aux(Board, Player, Move, ListOfMoves_0):-
+        Move=['up', 8],!,
+        valid_moves_make_list_aux(Board, Player, ['down',1], ListOfMoves_0).
+
+valid_moves_make_list_aux(Board, Player, Move, ListOfMoves_0):-
+        Move=['down', 8],!,
+        ListOfMoves_0=[].
+	
+valid_moves_make_list_aux(Board, Player,Move, ListOfMoves_0):-
+        Move=[Edge, X], 
+        ListOfMoves_0=[NewBoard | L2],
+        valid_move(Board, Player, Move, NewBoard),
+        X1 is (X + 1), 
+        valid_moves_make_list_aux(Board, Player, [Edge, X1], L2).
+	
+
+valid_moves_make_list(Board, Player, ListOfMoves_0):-
+	valid_moves_make_list_aux(Board, Player,['left',1], ListOfMoves_0).
 	
 	
 	
 cmp_lists(ListOfMoves, ListOfMoves_0):-
 	(ListOfMoves=[], ListOfMoves_0=[]);
-	(ListOfMoves_0=[0|L_02], cmp_lists(L_02, ListOfMoves_0)).
+	(ListOfMoves_0=[0|L_02], cmp_lists(ListOfMoves, L_02)).
 	
 cmp_lists(ListOfMoves, ListOfMoves_0):-
-	ListOfMoves_0=[X_0, L_02],
+	ListOfMoves_0=[X_0 | L_02],
 	X_0 \= 0, 
-	ListOfMoves=[X, L2],
+	ListOfMoves=[X | L2],
 	X=X_0,
 	cmp_lists(L2,L_02).
 	
@@ -70,4 +86,34 @@ cmp_lists(ListOfMoves, ListOfMoves_0):-
 valid_moves(Board, Player, ListOfMoves):-
 	valid_moves_make_list(Board, Player, ListOfMoves_0),
 	cmp_lists(ListOfMoves, ListOfMoves_0).
+
+display_boards(ListOfBoards):-
+        ListOfBoards=[0 | L2],
+	write(' 0 '),nl,
+	display_boards(L2).
+	
+display_boards(ListOfBoards):-
+	ListOfBoards=[Board | L2],
+	display_board(Board),
+	display_boards(L2).
+
+display_boards([]).
+
+test1:-
+        valid_moves([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],1,K),
+	display_boards(K).
+
+test22:-
+	valid_moves_make_list([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[1,1,1,1,1,1,1]],1,K),
+        display_boards(K).
+
+test2:-
+        valid_moves([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[1,1,1,1,1,1,1]],1,K),
+        display_boards(K).
+
+test3:-
+        valid_moves([[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[1,1,1,1,1,1,0]],1,K),
+        display_boards(K).
+
+	
 	
